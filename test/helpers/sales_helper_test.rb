7def2c4f7b653e8c5a3aca8037d4e79564df1da5
Shashank <singalshashank90@gmail.com>
2018-03-19 13:07:17 +0530

    adding test

--- test/helpers/sales_helper_test.rb
+++ test/helpers/sales_helper_test.rb
@@ -19,7 +19,7 @@ class SalesHelperTest < ActiveSupport::TestCase
     @sale.sale_items = [@sale_item1, @sale_item2]
   end
 
-  test "shall save new sale order and adjust article's stock and cost" do
+  test "shall save new sale order and adjust article's stock" do
     save_new_sales_order(@sale)
 
     @actual_article1 = Article.find(@article1.id)
@@ -35,5 +35,24 @@ class SalesHelperTest < ActiveSupport::TestCase
     assert_equal 'Article 2', @actual_article2.name
     assert_equal '10 KG', @actual_article2.package_quantity
     assert_equal 260, @actual_article2.mrp
+  end
+
+  test "shall save new sale order and adjust profit and loss" do
+    initial_pnl_balance = ProfitAndLossAccount.last.current_balance
+    save_new_sales_order(@sale)
+
+    latest_sale = Sale.last
+    assert_equal latest_sale.nature, 'CASH'
+    assert_equal latest_sale.village, 'Kurukshetra'
+    assert_equal latest_sale.phone, '9876543210'
+    assert_equal latest_sale.total_amount, 5_800
+    assert_equal latest_sale.sale_items.size, 2
+
+    latest_pnl_record = ProfitAndLossAccount.last
+
+    assert_equal initial_pnl_balance + 200 + 600, latest_pnl_record.current_balance
+    assert_equal 'From sale ' + @sale.id.to_s, latest_pnl_record.description
+    assert_equal 800, latest_pnl_record.amount
+    assert_not_empty latest_pnl_record.financial_year
   end
 end
